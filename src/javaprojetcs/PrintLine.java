fc1a77b8b5d394e8b285903d7af63bfd94e870ab
mohammed sikder <sikderismail@gmail.com>
2018-05-27 18:14:17 -0400

    added a class

---
+++ src/javaprojetcs/PrintLine.java
@@ -0,0 +1,11 @@
+package javaprojetcs;
+
+public class PrintLine {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I want to know everything about something");
+
+	}
+
+}
---
+++ src/javaprojetcs/PrintSomething.java
@@ -0,0 +1,10 @@
+package javaprojetcs;
+
+public class PrintSomething {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
