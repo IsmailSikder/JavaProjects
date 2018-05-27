94822575542709cef74c57616aa2b37e72a57984
mohammed sikder <sikderismail@gmail.com>
2018-05-27 19:44:20 -0400

    added a class with conditional statement

---
+++ src/condition/ConditionalStatement.java
@@ -0,0 +1,15 @@
+package condition;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		String name = "Ismail";
+		
+		if(name=="Ismail"){
+			System.out.println("My name is " + name);
+		}
+		else
+			System.out.println("My name is not" + name);
+	}
+
+}
