4487625d9dac9ade38016c4ad6dcdde7dfe81ac9
mohammed sikder <sikderismail@gmail.com>
2018-06-10 17:43:09 -0400

    adding a class on conditional statement

--- src/condition/ConditionalStatement.java
+++ src/condition/ConditionalStatement.java
@@ -1,15 +1,30 @@
+/*
+ * Find the largest number 
+ */
+
 package condition;
 
 public class ConditionalStatement {
 
 	public static void main(String[] args) {
 		String name = "Ismail";
+		int a=60, b=50, c=60;
 		
-		if(name=="Ismail"){
-			System.out.println("My name is " + name);
+		if(a>b&&a>c){
+			System.out.println("a is the larger number");
 		}
-		else
-			System.out.println("My name is not" + name);
+		
+		else if(b>a && b >c){
+			System.out.println("b is largest number");
+		}
+		else if(c==b)
+			System.out.println("c and b is largest number");
+		else if (c==a)
+			System.out.println("c and a is largest number");
+		else 
+			System.out.println("c is the largest number");
+		
+		
 	}
 
 }
